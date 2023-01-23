22f864d11228f86d1eefe20db92030c62d2a14fa
gadiribrahimov <iqq87@yahoo.com>
2023-01-22 22:51:18 -0800

    part1-decomposed

--- app-client-management/src/main/java/com/cydeo/service/UserClientService.java
+++ app-client-management/src/main/java/com/cydeo/service/UserClientService.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(value = "user-service",url = "http://localhost:9090")
 public interface UserClientService {
 
+
     @GetMapping("/api/v1/user/{username}")
     UserResponseDTO getUserDTOByUserName(@PathVariable("username") String username);
 }
